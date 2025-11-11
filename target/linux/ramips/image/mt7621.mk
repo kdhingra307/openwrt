54079185d208acde273a55f63da4df9c3df3ba06
Karan Dhingra <kdhingra307@gmail.com>
2025-11-11 19:19:41 +0000

    fix: different config
    Signed-off-by: Karan Dhingra <kdhingra307@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2840,18 +2840,36 @@ endef
 TARGET_DEVICES += tplink_archer-a6-v3
 
 define Device/tplink_archer-c5-v6-inact
+  $(Device/nand)
   $(Device/dsa-migration)
-  $(Device/tplink-safeloader)
-  TPLINK_BOARD_ID := Archer-C5-V6-INACT
+
   DEVICE_VENDOR := TP-Link
   DEVICE_MODEL := Archer C5
   DEVICE_VARIANT := v6 INACT
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7663 kmod-mt7663-firmware-ap kmod-usb3 kmod-mt7615e kmod-mt7615-common
-  KERNEL := $(KERNEL_DTB) | uImage lzma
-  KERNEL_INITRAMFS := $(KERNEL_DTB) | uImage lzma
+
+  TPLINK_BOARD_ID := Archer-C5-V6-INACT
+
+  DEVICE_PACKAGES := \
+	kmod-mt7603 \
+	kmod-mt7663 \
+	kmod-mt7663-firmware-ap \
+	kmod-usb3 \
+	kmod-mt7615e \
+	kmod-mt7615-common
+
+  KERNEL_IN_UBI := 1
+  KERNEL_LOADADDR := 0x82000000
+
+  # Kernel and Initramfs are both compressed and packed as FIT images
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+
+  # NAND flash → use UBI volumes instead of squashfs
+  IMAGE/sysupgrade.bin := sysupgrade-tar
+  IMAGE/factory.bin := append-kernel | pad-to 64k | append-ubi | check-size
+
   IMAGE_SIZE := 22528k
 endef
-TARGET_DEVICES += tplink_archer-c5-v6-inact
 
 define Device/tplink_archer-c6-v3
   $(Device/dsa-migration)
