778c57a776e9e8d78a6e034978835e66cc1db859
Karan Dhingra <kdhingra307@gmail.com>
2025-11-11 19:01:03 +0000

    fix: add tp link safeloaded
    Signed-off-by: Karan Dhingra <kdhingra307@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2841,6 +2841,7 @@ TARGET_DEVICES += tplink_archer-a6-v3
 
 define Device/tplink_archer-c5-v6-inact
   $(Device/dsa-migration)
+  $(Device/tplink-safeloader)
   TPLINK_BOARD_ID := Archer-C5-V6-INACT
   DEVICE_VENDOR := TP-Link
   DEVICE_MODEL := Archer C5
